a20f91e31719dd20a30bf8b1d0ce5faf0f00209a
Sultan-Alotaibi <sultan8761s@gmail.com>
2023-09-24 14:40:47 +0300

    ImageSlider

--- Sultan_pages/MainPage/JS/script.js
+++ Sultan_pages/MainPage/JS/script.js
@@ -4,7 +4,7 @@ document.addEventListener('scroll', function () {
     const loginBtn = document.querySelector('.login button');
     const links = document.querySelectorAll('.nav-link')
 
-    if (window.scrollY > 150) {
+    if (window.scrollY > 25 ) {
         navimg.src = '/Sultan_pages/IMG/logo.png';
         nav.classList.add('bg-white');
         loginBtn.classList.add('scrolled');
@@ -21,6 +21,31 @@ document.addEventListener('scroll', function () {
         });
     }
 })
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
 
 window.addEventListener('load', function () {
     const titles = [
